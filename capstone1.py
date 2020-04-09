f3b0ef9b346e729f5beaadf864d3a1f7882c45b7
Aditya Jaiswal <41647096+adijais19@users.noreply.github.com>
2020-04-09 06:42:45 +0530

    Update capstone1.py

--- capstone1.py
+++ capstone1.py
@@ -1,4 +1,4 @@
-This notebook will be mainly used the capstone project
+This notebook will be mainly used for the capstone project
 import pandas as pd
 import numpy as np
 print ('Hello Capstone Project Course!')
